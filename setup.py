8ce3354880471c1af25f04a4a6ec2eb4ea76273c
Kevin Shaju <kevinshaju01@gmail.com>
2025-08-05 17:05:40 +0800

    intial folder declaration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Kevin Shaju',
+    author_email= "pesmonu69@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
